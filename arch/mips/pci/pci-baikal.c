ca7961663b19975bbce6d817804821b7460c9e15
Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>
2017-09-19 12:55:56 +0300

    Fix PCIe initialization from non-reset state (kexec)
    Signed-off-by: Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>

--- arch/mips/pci/pci-baikal.c
+++ arch/mips/pci/pci-baikal.c
@@ -92,8 +92,6 @@ static int dw_pcie_get_busn(void)
    return PCIE_ROOT_BUS_NUM;
 }
 
-/* void pci_dw_dma_init(void); */
-
 void baikal_find_vga_mem_init(void);
 
 static uint32_t dw_pcie_phy_read(uint32_t phy_addr)
@@ -225,128 +223,22 @@ static int dw_pcie_init(void)
 	 * (PHY_RESET, PIPE_RESET, CORE_RST, PWR_RST, STICKY_RST, NONSTICKY_RST)
 	 */
 
-	/* PMU_PCIE_RSTC_REQ_RESET starts:
-	 * PIPE_RESET
-	 * CORE_RST
-	 * NON_STICKY_RST
-	 */
 	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	if ( reg & PMU_PCIE_RSTC_REQ_RESET ) {
-		/* PIPE_RESET */
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg |= PMU_PCIE_RSTC_PIPE_RESET;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-		usleep_range(1, 10);
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg &= ~PMU_PCIE_RSTC_PIPE_RESET;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-
-		/* CORE_RST */
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg |= PMU_PCIE_RSTC_CORE_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-		usleep_range(1, 10);
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg &= ~PMU_PCIE_RSTC_CORE_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-
-		/* NONSTICKY_RST */
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg |= PMU_PCIE_RSTC_NONSTICKY_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-		usleep_range(1, 10);
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg &= ~PMU_PCIE_RSTC_NONSTICKY_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-	}
-
-	/* PMU_PCIE_RSTC_REQ_PHY_RST ---> PMU_PCIE_RSTC_PHY_RESET */
-	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	if ( reg & PMU_PCIE_RSTC_REQ_PHY_RST ) {
-		reg |= PMU_PCIE_RSTC_PHY_RESET;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-		usleep_range(1, 10);
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg &= ~PMU_PCIE_RSTC_PHY_RESET;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-	}
-
-	/* PMU_PCIE_RSTC_REQ_CORE_RST ---> PMU_PCIE_RSTC_CORE_RST */
-	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	if ( reg & PMU_PCIE_RSTC_REQ_CORE_RST ) {
-		reg |= PMU_PCIE_RSTC_CORE_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-		usleep_range(1, 10);
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg &= ~PMU_PCIE_RSTC_CORE_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-	}
-
-	/* PMU_PCIE_RSTC_REQ_STICKY_RST ---> PMU_PCIE_RSTC_STICKY_RST */
-	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	if ( reg & PMU_PCIE_RSTC_REQ_STICKY_RST ) {
-		reg |= PMU_PCIE_RSTC_STICKY_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-		usleep_range(1, 10);
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg &= ~PMU_PCIE_RSTC_STICKY_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-	}
-
-	/* PMU_PCIE_RSTC_REQ_NON_STICKY_RST ---> PMU_PCIE_RSTC_NONSTICKY_RST */
-	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	if ( reg & PMU_PCIE_RSTC_REQ_NON_STICKY_RST ) {
-		reg |= PMU_PCIE_RSTC_NONSTICKY_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-		usleep_range(1, 10);
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg &= ~PMU_PCIE_RSTC_NONSTICKY_RST;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-	}
-
-	/* 5.1 set & clear individual bits */
-	/*
-	pmu_pcie_rstc_mask = 0;
-	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	if ( reg & PMU_PCIE_RSTC_REQ_RESET )
+	if (reg & PMU_PCIE_RSTC_REQ_PHY_RST)
 		pmu_pcie_rstc_mask |= PMU_PCIE_RSTC_PHY_RESET;
-	if ( reg & PMU_PCIE_RSTC_REQ_PHY_RST )
-		pmu_pcie_rstc_mask |= PMU_PCIE_RSTC_PHY_RESET;
-	if ( reg & PMU_PCIE_RSTC_REQ_CORE_RST )
+	if (reg & PMU_PCIE_RSTC_REQ_CORE_RST)
 		pmu_pcie_rstc_mask |= PMU_PCIE_RSTC_CORE_RST;
-	if ( reg & PMU_PCIE_RSTC_REQ_STICKY_RST )
+	if (reg & PMU_PCIE_RSTC_REQ_STICKY_RST)
 		pmu_pcie_rstc_mask |= PMU_PCIE_RSTC_STICKY_RST;
-	if ( reg & PMU_PCIE_RSTC_REQ_NON_STICKY_RST )
+	if (reg & PMU_PCIE_RSTC_REQ_NON_STICKY_RST)
 		pmu_pcie_rstc_mask |= PMU_PCIE_RSTC_NONSTICKY_RST;
-	if (pmu_pcie_rstc_mask) {
-		reg |= pmu_pcie_rstc_mask;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-		usleep_range(1, 10);
-		reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-		reg &= ~pmu_pcie_rstc_mask;
-		WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-	}
-	*/
-
-	/* 5.2 set & clear multiple bits */
-	/*pmu_pcie_rstc_mask = (PMU_PCIE_RSTC_PIPE_RESET | PMU_PCIE_RSTC_PWR_RST);*/
-	/* PMU_PCIE_RSTC_PIPE_RESET */
+	WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg | pmu_pcie_rstc_mask);
+	usleep_range(10, 20);
 	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	reg |= PMU_PCIE_RSTC_PIPE_RESET;
+	reg &= ~pmu_pcie_rstc_mask;
 	WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-	usleep_range(1, 10);
 	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	reg &= ~PMU_PCIE_RSTC_PIPE_RESET;
-	WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-	/* PMU_PCIE_RSTC_PWR_RST */
-	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	reg |= PMU_PCIE_RSTC_PWR_RST;
-	WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-	usleep_range(1, 10);
-	reg = READ_PMU_REG(BK_PMU_PCIE_RSTC);
-	reg &= ~PMU_PCIE_RSTC_PWR_RST;
-	WRITE_PMU_REG(BK_PMU_PCIE_RSTC, reg);
-
+	pr_debug("%s: PCIE_RSTC after reset: %08x\n", __func__, reg);
 
 	/* 3.1 Set DBI2 mode, dbi2_cs = 0x1 */
 	reg = READ_PMU_REG(BK_PMU_PCIE_GENC);
