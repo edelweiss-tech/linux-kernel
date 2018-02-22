9fae20cbb203dc7dda3904340b64db0744b2ccc7
Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>
2018-02-22 16:19:48 +0300

    [dw_wdt] Fix timeout calculation for Baikal MIPS SoC
    Signed-off-by: Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>

--- drivers/watchdog/dw_wdt.c
+++ drivers/watchdog/dw_wdt.c
@@ -73,7 +73,11 @@ static inline int dw_wdt_top_in_seconds(struct dw_wdt *dw_wdt, unsigned top)
 	 * There are 16 possible timeout values in 0..15 where the number of
 	 * cycles is 2 ^ (16 + i) and the watchdog counts down.
 	 */
+#ifndef CONFIG_MIPS_BAIKAL
 	return (1U << (16 + top)) / dw_wdt->rate;
+#else
+	return (1U << (12 + top)) / dw_wdt->rate;
+#endif
 }
 
 static int dw_wdt_get_top(struct dw_wdt *dw_wdt)
