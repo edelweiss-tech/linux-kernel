1d309d15460d44487a3154f4ce5ce62fda0ba4c9
Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>
2018-09-11 18:57:20 +0300

    DRM: Writecombine mapping in drm_vm for mips
    Signed-off-by: Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>

--- drivers/gpu/drm/drm_vm.c
+++ drivers/gpu/drm/drm_vm.c
@@ -58,7 +58,7 @@ static pgprot_t drm_io_prot(struct drm_local_map *map,
 {
 	pgprot_t tmp = vm_get_page_prot(vma->vm_flags);
 
-#if defined(__i386__) || defined(__x86_64__) || defined(__powerpc__)
+#if defined(__i386__) || defined(__x86_64__) || defined(__powerpc__) || defined(__mips__)
 	if (map->type == _DRM_REGISTERS && !(map->flags & _DRM_WRITE_COMBINING))
 		tmp = pgprot_noncached(tmp);
 	else
@@ -69,7 +69,7 @@ static pgprot_t drm_io_prot(struct drm_local_map *map,
 		tmp = pgprot_writecombine(tmp);
 	else
 		tmp = pgprot_noncached(tmp);
-#elif defined(__sparc__) || defined(__arm__) || defined(__mips__)
+#elif defined(__sparc__) || defined(__arm__)
 	tmp = pgprot_noncached(tmp);
 #endif
 	return tmp;
